d9c91e496b33a97bcfc3e0e32b8ea47ad98837bd
гребенюкмакс <148119706+grebeniukmax@users.noreply.github.com>
2024-11-01 11:57:01 +0200

    JS-Homework-1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
+"use strict"
+//* Перше завдання //
 let age = 14;
 console.log(age);
-let name = "Максим";
+//* Друге завдання //
+let name = "Max";
 console.log(name);
-const student = true
-console.log(student)
+//* Трете завдання //
+const isStudent = true
+console.log(isStudent)
+//* Четверте завдання //
+let lovelyPhrase = "Один за всіх і всі за одного"
+console.log(lovelyPhrase)
+//* П'яте завдання //
+var firstNumber = 20
+var secondNumber = 10
+var myNumber = firstNumber + secondNumber
+console.log(myNumber)
+//* Шосте завдання //
+const myNull = null
+console.log(myNull)
+//* Сьоме завдання //
+//! Я не знаю як зробити щоб після введення імя висвічувалося імя користувача яке він вів //
+let userName = prompt ("Введіть свое імя");
+if (userName) {
+alert ("Привіт,...!")}
+else {
+alert ("Введіть ім'я будь ласка")
+}
+//* Восьме завдання //
+let confirming = confirm ("Підтверджуете дію?")
+if (confirming) {
+    alert ("Дякую за підтверження!")
+}
+else {
+    alert ("Дію відмінено!")
+}
+//* Дев'яте завдання //
+alert("Дія може бути небезпечною");
+let confirmation = "Підтвердіть дію"
+if (confirmation) {
+    alert("Дякую за підтвердження!");
+}
+else {
+    alert("Дію успішно відмінено!");
+}
